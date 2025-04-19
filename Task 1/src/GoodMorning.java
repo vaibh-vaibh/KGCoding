e5be6179784602ee0cdd859351ff9956b8c97eea
vaibh-vaibh <vaibhav16032005@gmail.com>
2025-04-19 15:40:07 +0530

    Initial Java KGCoding Upload

---
+++ Task 1/src/GoodMorning.java
@@ -0,0 +1,7 @@
+public class GoodMorning 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("good morning");
+	}
+}
---
+++ Task 1/src/LeftHalfPyramid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeftHalfPyramid 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int s=n-1;s>=i;s--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
